49555b12db6bd90f2e950beb84a8341094028dd7
erwinwahyura <you@example.com>
2017-06-20 16:50:37 +0700

    initialize app and mongoose connection

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/erwin-mongoose');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('connection success!!!');
+});
+
+var index = require('./routes/index');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', index);
+
+app.listen(3000)
+
+module.exports = app;
